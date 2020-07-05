eb2332f2c3c7adcaaf1566d2327c24f2ae5a7fc3
Joel Roske <joelroske@Owners-MacBook-Air.local>
2020-07-05 00:31:37 -0500

    highlight when complete

--- server/modules/tasks.router.js
+++ server/modules/tasks.router.js
@@ -8,7 +8,7 @@ const pool = require('./pool');
 taskRouter.get('/', (req, res) => {
     console.log('/tasks GET');
     /// - query: SELECT * FROM "tasks" - ///
-    let queryString = `SELECT * FROM todo`;
+    let queryString = `SELECT * FROM todo ORDER BY "id";`;
     pool.query(queryString).then((result) => {
         // success
         res.send(result.rows);
@@ -22,8 +22,8 @@ taskRouter.get('/', (req, res) => {
 
 taskRouter.post('/', (req, res) => {
     console.log('in /tasks POST:', req.body);
-    let queryString = `INSERT INTO "todo" ( "complete", "task" ) 
-        VALUES ( false, $1 )`; //task entered will be automatically considered incomplete
+    let queryString = `INSERT INTO "todo" ( "task" ) 
+        VALUES ( $1 )`; //task entered will be automatically considered incomplete
     pool.query(queryString,
         [req.body.task ]).then((result) => {
         res.sendStatus( 201 );
@@ -34,12 +34,23 @@ taskRouter.post('/', (req, res) => {
 })
 //PUT
 
+taskRouter.put('/:id', (req, res) => {
+    console.log('/tasks PUT:', req.params.id);
+    res.send('whinny');
+    let queryString = `UPDATE todo SET complete = true WHERE id = $1;`;
+    pool.query(queryString, [req.params.id]).then((results) => {
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+});
+
 //DELETE
 taskRouter.delete('/:id', (req, res) => {
     console.log('/tasks DELETE hit:', req.params.id);
     //have pool run a delete
-    let queryString = `DELETE FROM "todo" WHERE "id"=${req.params.id};`;
-    pool.query(queryString).then((results) => {
+    let queryString = `DELETE FROM "todo" WHERE "id"=$1;`;
+    pool.query(queryString, [req.params.id]).then((results) => {
         res.sendStatus( 201 );
     }).catch((err) => {
         console.log('problem');
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,7 @@ function onReady() {
     console.log('in onReady');
     $('#btn-add').on('click', addTask);
     getTasks();
-    $('#tasksOut').on('click', '.btn-deleteTask', deleteTask);
+    $('.completeTasksOut').on('click', '.btn-deleteTask', deleteTask);
     $('#tasksOut').on('click', '.btn-completeTask', completeTask);
 
 }
@@ -26,7 +26,7 @@ function addTask() {
         data: taskToSend
     }).then(function (response) {
         console.log('back from POST:', response);
-        getTasks();
+        //getTasks();
         //empty inputs
         $("#taskIn").val("");
     }).catch(function (err) {
@@ -43,23 +43,30 @@ function getTasks() {
         url: '/tasks'
     }).then(function (response) {
         console.log('back from GET:', response);
-        let el = $('#tasksOut');
-        el.empty();
+        $('#tasksOut').empty();
+        $('.completeTasksOut').empty();
         // tasks = response;
         for (let i = 0; i < response.length; i++) {
             // in our button we are using "data-id" to hold the id of each bird
             //if statement for if ready, no append button, if false, append ready button
             //<td><button class="btn-completeTask" data-id=${ tasks[i].id}>Complete</button></td>
+            //  if (response[i].complete === true){
+            //     closest('tr').addClass('highlightYellow');
+            // }
             let transfer = `${response[i].complete}`;
             if (response[i].complete === false) {
-                transfer = `<button class="btn-completeTask" data-id=${response[i].id}>Complete</button>`
-            }
-            el.prepend(`<tr>
-            <td>${transfer}</td>              
+                $('#tasksOut').prepend(`<tr>
+                <td><button class="btn-completeTask" data-id=${response[i].id}>Complete</button></td>              
+                <td>${response[i].task}</td>
+                <td><button class="btn-deleteTask" data-id=${ response[i].id}>Delete</button></td>
+              </tr>`)
+            } else {
+            $('.completeTasksOut').prepend(`<tr>
+            <td>Completed</td>              
             <td>${response[i].task}</td>
             <td><button class="btn-deleteTask" data-id=${ response[i].id}>Delete</button></td>
           </tr>`)
-        }
+        }}
     }).catch(function (err) {
         console.log(err);
         alert('nope');
@@ -67,9 +74,23 @@ function getTasks() {
 }
 
 function completeTask() {
-    console.log('in completeTask');
+    console.log('in completeTask', $(this).data('id'));
+    const completeTask = $(this).data('id');
+    $(this).closest('tr').addClass('highlightYellow');
+    console.log('the koala that is ready is:', completeTask);
+    $.ajax({
+      method: 'PUT',
+      url: `/tasks/` + completeTask,
+      data: { complete: true }
+    }).then(function (response) {
+    getTasks();
+      console.log('back from PUT with:', response);
+    }).catch(function (error) {
+      alert('error with PUT completion. see console for details');
+      console.log(error);
+    })
+  }
 
-}
 
 function deleteTask() {
     console.log('in deleteTask');
